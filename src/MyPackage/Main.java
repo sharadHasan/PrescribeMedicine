4210f554c70e2f9254a6bc7f2cd834c1895a3698
sharadHasan <hasansharad@gmail.com>
2018-04-28 01:30:43 +0600

    Changing Main

--- src/MyPackage/Main.java
+++ src/MyPackage/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            //DBConnection myDB = new DBConnection();
+
             PatientRecord pr = new PatientRecord();
         } catch (SQLException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
