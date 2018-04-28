353ff030e6e8686f890a0c17f9e9a1bdcf53455e
muiduzzaman <37136319+muiduzzaman@users.noreply.github.com>
2018-04-28 08:34:23 +0600

    Update DBConnection.java

--- src/MyPackage/DBConnection.java
+++ src/MyPackage/DBConnection.java
@@ -3,8 +3,6 @@ package MyPackage;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class DBConnection {
     Connection connection = null;
@@ -12,9 +10,8 @@ public class DBConnection {
     public DBConnection(){
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PatientRecord", "root", "");
-            //System.out.println("Connection Successful");
-        } catch (SQLException ex) {
-            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
